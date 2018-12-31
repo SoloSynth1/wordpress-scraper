4b6ca22cb7daeff5d770e4181db886ebb4e3f857
Orix <orix.auyeung@hunt.social>
2018-12-31 14:37:11 +0800

    add new sites

--- chinainternetwatch.com.py
+++ chinainternetwatch.com.py
@@ -1,20 +1,20 @@
 from crawler import crawler
 import os
 
-url = "https://www.tripwire.com/state-of-security"
-output_dir = os.path.join('.', 'data', 'tripwire.com')
+url = "https://www.chinainternetwatch.com"
+output_dir = os.path.join('.', 'data', 'chinainternetwatch.com')
 headers = {
         'Accept':'application/json, text/javascript, */*; q=0.01',
         'Accept-Encoding':'*',
         'Accept-Language':'zh-CN,zh;q=0.8',
-        'Cookie': 'ASP.NET_SessionID=rno4pepppbkdh2f1bn5nc2kj; SC_ANALYTICS_GLOBAL_COOKIE=f9c357b4f7124f7890bd980c59b97db4|False; active=yes; Set_Me=3415655320.1.1933537720.2330721216; SnapABugRef=https%3A%2F%2Fwww.tripwire.com%2F%20; SnapABugHistory=1#; SnapABugVisit=1#1541675065',
+        'Cookie': '_ga=GA1.2.1769368666.1545970795; _hjIncludedInSample=1; _gid=GA1.2.240454939.1546227015; _fbp=fb.1.1546227015921.374506551; PHPSESSID=a856739d604f1496cd355f2dc35f3371',
         'Connection':'keep-alive',
         'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/70.0.3538.67 Chrome/70.0.3538.67 Safari/537.36',
         'X-Requested-With':'XMLHttpRequest'
         }
 
 if __name__ == "__main__":
-    wpc = crawler.WordPressCrawler(url, headers, crawl_rate=25)
+    wpc = crawler.WordPressCrawler(url, headers, crawl_rate=100)
     wpc.get_tags(os.path.join('{}'.format(output_dir), 'tags.json'))
     wpc.get_categories(os.path.join('{}'.format(output_dir), 'cats.json'))
     wpc.get_posts(os.path.join('{}'.format(output_dir), 'posts.json'))
