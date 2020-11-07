e4d71e341fb5dd415213148668acfe2816ccc390
SoloSynth1 <orixay@hotmail.com>
2020-11-08 00:45:59 +0800

    rewrite crawling procedure

--- wpscraper/crawler.py
+++ wpscraper/crawler.py
@@ -1,66 +1,53 @@
 import json
 import time
-import os
-import re
 from abc import ABC, abstractmethod
-from typing import List
+
+from wpscraper.headers import Headers
+from wpscraper.document import RawDocument
 
 import requests
 
 
-
 class Crawler(ABC):
+    def __init__(self, url, headers: Headers, verify_ssl: bool, timeout: int, max_retries: int):
+        self.api_path = url + '/wp-json/wp/v2'
+        self.headers = headers
+        self.verify_ssl = verify_ssl
+        self.timeout = timeout
+        self.max_retries = max_retries
+
     @abstractmethod
-    def crawl(self):
+    def crawl(self, resource: str, *args, **kwargs):
         pass
 
-class WordPressCrawler(Crawler):
-    def __init__(self, url, headers: Header = None,, crawl_rate, verify_ssl):
-        self.api = url+'/wp-json/wp/v2'
-        self.headers = headers
+
+class SimpleRequestsCrawler(Crawler):
+    def __init__(self, url, headers: Headers = None, crawl_rate: int = 25, verify_ssl: bool = True, timeout: int = 30,
+                 max_retries: int = 5, constant_retry_standoff: int = 30):
+        super().__init__(url=url, headers=headers, verify_ssl=verify_ssl, timeout=timeout, max_retries=max_retries)
+        self.crawled_resource_count = {}
         self.crawl_rate = crawl_rate
-        self.verify_ssl = verify_ssl
-        self.resource_to_crawl = resource_to_crawl
-        self.page = 0
-        # self.session = HTMLSession()
+        self.constant_retry_standoff = constant_retry_standoff
 
-    def _abstract_get(self, path, output_file):
-        json_output = self._crawl_jsons(self.api + path)
-        if output_file:
-            utils.dump_json(json_output, output_file)
-        return json_output
+    def crawl(self, resource: str):
+        documents = []
+        if resource in self.crawled_resource_count.keys():
+            self.crawled_resource_count[resource] = 0
+        objs = self._get_json_response(
+            self.api_path+'?per_page={}&page={}'.format(self.crawl_rate, self.crawled_resource_count[resource])
+        )
+        if objs and isinstance(objs, list):
+            documents = [RawDocument(obj) for obj in objs]
+            self.crawled_resource_count[resource] += 1
+        else:
+            print("No documents are crawled.")
+        return documents
 
-    def get_categories(self, output_file=None):
-        return self._abstract_get('/categories', output_file)
-
-    def get_tags(self, output_file=None):
-        return self._abstract_get('/tags', output_file)
-
-    def get_posts(self, output_file=None):
-        return self._abstract_get('/posts', output_file)
-
-    def _isjsonarray(self, json):
-        return json and isinstance(json, list)
-
-    def _crawl_jsons(self, url):
-        output = []
-        i = 1
-        while True:
-            json_repsonse = self._get_json_response('{}?per_page={}&page={}'.format(url, self.crawl_rate, i))
-            if self._isjsonarray(json_repsonse):
-                output += json_repsonse
-                i += 1
-            else:
-                break
-        return output
-
-    def _get_json_response(self, url, max_retries=5, retry_standoff_time=30):
-        retries = 0
-        while True:
+    def _get_json_response(self, url):
+        attempt_count = 1
+        while attempt_count < self.max_retries:
             try:
-                # response = self.session.get(url, headers=self.headers, timeout=30)
-                # # response.html.render()
-                response = requests.get(url, headers=self.headers, timeout=30, verify=self.verify_ssl)   # 30 seconds
+                response = requests.get(url, headers=self.headers, timeout=self.timeout, verify=self.verify_ssl)
                 print('subpath: {}'.format(url))
                 print('response code: {}'.format(response.status_code))
                 print('response head: {}'.format(response.text[:300]))
@@ -68,40 +55,20 @@ class WordPressCrawler(Crawler):
                     return json.loads(next(response.iter_lines()))
                 else:
                     print("status code returned {}".format(response.status_code))
-                    retries += 1
-                    if retries < max_retries:
-                        print("waiting for {} seconds...".format(retry_standoff_time))
-                        time.sleep(retry_standoff_time)
-                        print("retrying... (attempt {} of {})".format(retries, max_retries))
-                    else:
-                        print("max no. of retries reached. exiting...")
-                        break
             except requests.Timeout:
                 print("Timed out.")
                 continue
             except Exception as e:
                 print("Exception occurred: {}".format(e))
                 continue
+            finally:
+                attempt_count += 1
+                print("waiting for {} seconds...".format(self.constant_retry_standoff))
+                time.sleep(self.constant_retry_standoff)
+                print("retrying... (attempt {} of {})".format(attempt_count, self.max_retries))
+        print("max no. of retries reached. exiting...")
+        return None
 
+    def get_crawled_stat(self):
+        return self.crawled_resource_count
 
-def ensure_file_directory(file):
-    inception(os.path.dirname(os.path.realpath(file)))
-
-
-def inception(directory):
-    parent_directory = os.path.dirname(directory)
-    if not os.path.exists(parent_directory):
-        inception(parent_directory)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-
-def dump_json(json_object, output_file):
-    ensure_file_directory(output_file)
-    with open(output_file, 'w') as f:
-        f.write(json.dumps(json_object))
-
-
-def remove_leading_scripts(response_text):
-    cleaned_text = re.search(r'(\[.*)', response_text)
-    return cleaned_text.group(1)
