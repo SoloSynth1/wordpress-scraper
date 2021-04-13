39f65988c882a5541505f4145296bf339b22a687
SoloSynth1 <orixay@hotmail.com>
2021-04-13 18:14:22 +0800

    add multiprocessing in legacy_crawl_all.py

--- legacy_crawl_all.py
+++ legacy_crawl_all.py
@@ -1,21 +1,30 @@
 import csv
 import os
+from multiprocessing import Pool
 
 from legacy_main import crawl
+
+threads = 5
+
+
+def multiple_crawl(jobs):
+    flattened_jobs = map(lambda x: (x['url'], x['output_dir']), jobs)
+    with Pool(threads) as p:
+        p.starmap(crawl, flattened_jobs)
 
 
 def parse(csv_path: str):
     if os.path.exists(csv_path):
         with open(csv_path, 'r') as f:
             csvreader = csv.DictReader(f, delimiter=',', quotechar='"')
-            for row in csvreader:
-                crawl(row['url'], row['output_dir'])
+            return list(csvreader)
     else:
         raise FileNotFoundError()
 
 
 def main():
-    parse("./legacy/scripts/crawled_list.csv")
+    jobs = parse("./legacy/scripts/crawled_list.csv")
+    multiple_crawl(jobs)
 
 
 if __name__ == "__main__":
