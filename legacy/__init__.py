7842d986e92565d2b35e2268565ef557239b6d22
SoloSynth1 <orixay@hotmail.com>
2021-04-13 14:12:19 +0800

    modify legacy_main.py to fix module not found error

--- legacy/__init__.py
+++ legacy/__init__.py
@@ -1,3 +1,14 @@
+common_header = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': '*',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+    'Cookie': '_ga=GA1.2.1769368666.1545970795; _hjIncludedInSample=1; _gid=GA1.2.240454939.1546227015; _fbp=fb.1.1546227015921.374506551; PHPSESSID=a856739d604f1496cd355f2dc35f3371',
+    'Connection': 'keep-alive',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/70.0.3538.67 Chrome/70.0.3538.67 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
+
+
 def common_crawl(wpc):
     wpc.get_tags()
     wpc.get_categories()
--- legacy_main.py
+++ legacy_main.py
@@ -1,15 +1,15 @@
 import argparse
-
-from legacy.crawler import BasicJSONCrawlerManager
+from legacy.crawler import crawler
 
 if __name__ == "__main__":
+    from legacy import common_header, common_crawl
     parser = argparse.ArgumentParser()
     parser.add_argument("url")
     parser.add_argument("output_dir")
     parser.add_argument("--crawl_rate", default=25)
     args = parser.parse_args()
     if args.crawl_rate:
-        manager = BasicJSONCrawlerManager(args.url, args.output_dir, int(args.crawl_rate))
+        wpc = crawler.WordPressCrawler(args.url, common_header, args.output_dir, int(args.crawl_rate))
     else:
-        manager = BasicJSONCrawlerManager(args.url, args.output_dir)
-    manager.crawl()
+        wpc = crawler.WordPressCrawler(args.url, common_header, args.output_dir)
+    common_crawl(wpc)
