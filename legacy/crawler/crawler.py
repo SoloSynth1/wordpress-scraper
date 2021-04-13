6af55367bcdcf5ec4f94b5837fc6be8006b502ac
SoloSynth1 <orixay@hotmail.com>
2021-04-13 18:13:59 +0800

    revise legacy WordPressCrawler to better handle retries

--- legacy/crawler/crawler.py
+++ legacy/crawler/crawler.py
@@ -56,30 +56,27 @@ class WordPressCrawler:
         return output
 
     def _get_json_response(self, url, max_retries=5, retry_standoff_time=30):
-        retries = 0
-        while True:
+        retries = 1
+        while retries <= max_retries:
+            print("attempt #{} of {} - {}".format(retries, max_retries, url))
             try:
-                # response = self.session.get(url, headers=self.headers, timeout=30)
-                # # response.html.render()
                 response = requests.get(url, headers=self.headers, timeout=30, verify=self.verify_ssl)  # 30 seconds
-                print('subpath: {}'.format(url))
                 print('response code: {}'.format(response.status_code))
                 print('response head: {}'.format(response.text[:300]))
-                if response.status_code == 200 or response.status_code == 400:
-                    return json.loads(next(response.iter_lines()))
+                # some API return valid response despite code 400 (weird I know)
+                if response.status_code <= 400:
+                    # return json.loads(response.iter_lines())
+                    return json.loads(response.content.decode('utf-8').strip('\n').strip(' '))
                 else:
                     print("status code returned {}".format(response.status_code))
-                    retries += 1
-                    if retries < max_retries:
-                        print("waiting for {} seconds...".format(retry_standoff_time))
-                        time.sleep(retry_standoff_time)
-                        print("retrying... (attempt {} of {})".format(retries, max_retries))
-                    else:
-                        print("max no. of retries reached. exiting...")
-                        break
             except requests.Timeout:
                 print("Timed out.")
                 continue
             except Exception as e:
                 print("Exception occurred: {}".format(e))
                 continue
+            retries += 1
+            print("waiting for {} seconds...".format(retry_standoff_time))
+            time.sleep(retry_standoff_time)
+        print("max no. of retries reached. exiting...")
+        return None
